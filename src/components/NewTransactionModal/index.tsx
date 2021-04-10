298a6b985fdac73bac806dace5ab1bab00042275
Gustavo Ribeiro <gugahribeiro@hotmail.com>
2021-04-10 19:57:49 -0300

    chore: add close modal button

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useCallback } from 'react';
 
 import { Container } from './styles';
+import closeIcon from '../../assets/close.svg';
 
 import Modal from 'react-modal';
 
@@ -17,7 +18,14 @@ const NewTransactionModal: React.FC<NewTransactionModalProps> = ({ isOpen, onReq
             className='react-modal-content'
             isOpen={isOpen}
             onRequestClose={onRequestClose}
-        >
+        >   
+        <button 
+            type='button' 
+            onClick={onRequestClose}
+            className='react-modal-close'
+            >
+            <img src={closeIcon} alt="Fechar modal"/>
+        </button>
             <Container>
                 <h1>Cadastrar transação</h1>
                 <input type="text" placeholder='Nome'/>
